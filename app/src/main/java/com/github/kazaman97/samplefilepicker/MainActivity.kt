ac1f1b2aa963674d3b9d83de842e0733b5744d32
kazaman97 <narusai97@gmail.com>
2021-11-10 23:11:22 +0900

    add text file open picker

--- app/src/main/java/com/github/kazaman97/samplefilepicker/MainActivity.kt
+++ app/src/main/java/com/github/kazaman97/samplefilepicker/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.github.kazaman97.samplefilepicker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.github.kazaman97.samplefilepicker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    private val content =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            binding.filePath.text = uri?.toString()
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding.fileSelectionButton.setOnClickListener {
+            content.launch("text/*")
+        }
     }
 }
